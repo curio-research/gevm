2986e8de99f569455a18f0e81869f509f5516734
daweth <46637826+daweth@users.noreply.github.com>
2023-10-20 11:09:35 -0700

    add separate types package

---
+++ gevmtypes/types.go
@@ -0,0 +1,33 @@
+package gevmtypes
+
+// request is a JSON RPC request package assembled internally from the client
+// method calls.
+type Request struct {
+	JsonRpc string        `json:"jsonrpc"` // Version of the JSON RPC protocol, always set to 2.0
+	Id      int           `json:"id"`      // Auto incrementing ID number for this request
+	Method  string        `json:"method"`  // Remote procedure name to invoke on the server
+	Params  []interface{} `json:"params"`  // List of parameters to pass through (keep types simple)
+}
+
+// response is a JSON RPC response package sent back from the API server.
+type Response struct {
+	JsonRpc string `json:"jsonrpc"` // Version of the JSON RPC protocol, always set to 2.0
+	Id      int    `json:"id"`      // Auto incrementing ID number for this request
+	Error   []byte `json:"error"`   // Any error returned by the remote side
+	Result  []byte `json:"result"`  // Whatever the remote side sends us in reply
+	GasLeft uint64 `json:"gasLeft"` // Gas left over from the transaction
+}
+
+// transaction is the data payload from the caller
+type Transaction struct {
+	From     string `json:"from"`
+	To       string `json:"to"`
+	Gas      uint64 `json:"gas"`
+	GasPrice uint64 `json:"gasPrice"`
+	Value    uint64 `json:"value"`
+	Data     string `json:"data"`
+}
+
+type BlockNumber string
+
+type Address string
