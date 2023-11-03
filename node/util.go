09fa9d40c358340b5d22a4eaabe05197e45c40b8
daweth <46637826+daweth@users.noreply.github.com>
2023-11-02 20:39:17 -0400

    add bytesToUint conversion fn

--- node/util.go
+++ node/util.go
@@ -8,3 +8,11 @@ func Uint64ToBytes(num uint64) []byte {
 	binary.BigEndian.PutUint64(bytes, num)
 	return bytes
 }
+
+// BytesToUint64 converts a slice of 8 bytes to a uint64
+func BytesToUint64(bytes []byte) uint64 {
+	if len(bytes) < 8 {
+		panic("byte slice is too short to be converted to uint64")
+	}
+	return binary.BigEndian.Uint64(bytes)
+}
