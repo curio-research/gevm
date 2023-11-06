d25f0dc7d648fe4fc1db727a388afa7d13d244ff
daweth <46637826+daweth@users.noreply.github.com>
2023-11-06 17:13:02 -0500

    add raw tx and interface reflection helpers

--- node/util.go
+++ node/util.go
@@ -16,3 +16,36 @@ func BytesToUint64(bytes []byte) uint64 {
 	}
 	return binary.BigEndian.Uint64(bytes)
 }
+
+// RawTxToTxObject converts a []bytes transaction to a transaction struct
+func RawTxToTxObject(rawTxHex string) gevmtypes.Transaction {
+	// example
+	// rawTxHex := "0xf86b808502540be4008252089411111111111111111111111111111111111111111880de0b6b3a7640000801ba0b3ef7e65d0516b4a5b1db67e49b1d14d9c3e44e6fe288997560e6099cfec5c17a07e094d2502257f2d3a8d4eeb2e2d5a7170b2f1c5b6d8e6f6a5a8d87c465dd771b3a"
+
+	// Strip the 0x prefix if present
+	if rawTxHex[:2] == "0x" {
+		rawTxHex = rawTxHex[2:]
+	}
+
+	// Decode the hex string to a byte slice
+	rlpBytes, err := hex.DecodeString(rawTxHex)
+	if err != nil {
+		log.Fatalf("Failed to decode hex string: %v", err)
+	}
+
+	// RLP decode the byte slice back into a Transaction
+	var tx gevmtypes.Transaction
+	err = rlp.DecodeBytes(rlpBytes, &tx)
+	if err != nil {
+		log.Fatalf("Failed to RLP decode transaction: %v", err)
+	}
+
+	fmt.Printf("Decoded TX: %+v\n", tx)
+	return tx
+}
+
+// check that an interface is a string
+func interfaceIsString(i interface{}) bool {
+	_, ok := i.(string)
+	return ok
+}
