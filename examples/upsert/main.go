a19ac526ccdd5a40ceec341942fcceb2a1f577bc
daweth <46637826+daweth@users.noreply.github.com>
2023-11-22 17:51:30 -0500

    add script template

--- examples/upsert/main.go
+++ examples/upsert/main.go
@@ -7,16 +7,18 @@ import (
 	"os"
 	"time"
 
-	// "github.com/daweth/gevm/vm"
+	ec "github.com/daweth/gevm/core"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/vm"
+	// gm "github.com/ethereum/go-ethereum/common/math"
 )
 
 var (
 	testAddress      = common.HexToAddress("alice")
-	toAddress        = common.HexToAddress("bob")
+	toAddress    = common.HexToAddress("bob")
 	uncreatedAddress = common.HexToAddress("vitalik")
 	amount           = big.NewInt(1)
 	accountNonce     = uint64(0)
@@ -51,15 +53,63 @@ func getTPS(start time.Time, end time.Time) int64 {
 }
 
 func main() {
+	binFilePath := "./upsert.bin"
+	abiFilePath := "./upsert.abi"
+	data := loadBin(binFilePath)
+	abiObj := loadAbi(abiFilePath)
+
 	alice, err := testAddress.MarshalText()
 	must(err)
 	vitalik, err := uncreatedAddress.MarshalText()
 	must(err)
 
-	// node := ec.NewNodeContext(gasLimit, gasUsed, testAddress, toAddress)
+	node := ec.NewNodeContext(gasLimit, gasUsed, testAddress, toAddress)
 	fmt.Println("Alice Addr=", alice)
 	fmt.Println("Vitalik Addr=", vitalik)
 
-	
+	// creating the contract
+	fmt.Println("balance: ", node.StateDB.GetBalance(testAddress).Uint64())
+	contractRef := vm.AccountRef(testAddress)
+	contractCode, contractAddress, gasLeftOver, vmerr := node.Evm.Create(contractRef, data, node.StateDB.GetBalance(testAddress).Uint64(), big.NewInt(0))
+	must(vmerr)
 
+	fmt.Println("contract address=", contractAddress)
+
+	// fund the account
+	node.StateDB.SetBalance(testAddress, big.NewInt(0).SetUint64(gasLeftOver))
+	testBalance := node.StateDB.GetBalance(testAddress)
+	fmt.Println("after contract creation, testBalance=", testBalance, contractCode)
+
+	// calling the contract
+	method := abiObj.Methods["sendETH"]
+	pm := vitalik
+	input := append(method.ID, pm...)
+	fmt.Println(hexutil.Encode(input))
+
+	startTime := time.Now()
+	fmt.Println("begin to exec contract")
+	node.StateDB.SetCode(testAddress, contractCode)
+	outputs, gasLeft, vmerr := node.Evm.Call(contractRef, testAddress, input, node.StateDB.GetBalance(testAddress).Uint64(), big.NewInt(0))
+	must(vmerr)
+	endTime := time.Now()
+
+	executionTime := endTime.Sub(startTime)
+	fmt.Printf("function executed in %v nanoseconds\n", executionTime.Nanoseconds())
+
+	tps := getTPS(startTime, endTime)
+	fmt.Printf("Theoretical TPS is %v\n", tps)
+
+	node.StateDB.SetBalance(testAddress, big.NewInt(0).SetUint64(gasLeft))
+	testBalance = node.StateDB.GetBalance(testAddress)
+	fmt.Println("after call contract, testBalance =", testBalance)
+
+	for _, op := range method.Outputs {
+		switch op.Type.String() {
+		case "uint256":
+			fmt.Printf("Output name=%s, value=%d\n", op.Name, big.NewInt(0).SetBytes(outputs))
+		default:
+			fmt.Println(op.Name, op.Type.String())
+		}
+	}
+	fmt.Printf("Output %#v\n", hexutil.Encode(outputs))
 }
